f0206bc5a3757e24c0e799e57841c16848b507e1
Philip Combiths <philcombiths@gmail.com>
2023-06-17 08:20:17 -0500

    Update reorganizePhonProject.py

--- reorganizePhonProject.py
+++ reorganizePhonProject.py
@@ -1,4 +1,3 @@
-
 """
 With a specified Phon Project directory containing at least one corpus,
 generates new corpora and filters Phon sessions into those corpora
@@ -22,7 +21,7 @@ import re
 import shutil
 from contextlib import contextmanager
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 
 # Create contextmanagers
@@ -30,7 +29,7 @@ from contextlib import contextmanager
 def change_dir(newdir):
     prevdir = os.getcwd()
     try:
-        yield os.chdir(os.path.expanduser(newdir)) 
+        yield os.chdir(os.path.expanduser(newdir))
     finally:
         os.chdir(prevdir)
 
@@ -39,163 +38,80 @@ def change_dir(newdir):
 def enter_dir(newdir):
     prevdir = os.getcwd()
     try:
-        yield os.chdir(newdir) 
+        yield os.chdir(newdir)
     finally:
         os.chdir(prevdir)
-        
-def organize_files_by_keyword(keyword):
-    """Create a new corpus filtered to session names containing keywork.
 
-    Args:
-        keyword (STR): String to use as corpus name and session filter.
-    """
-    # Working in corpus folder
-    projectDir = os.path.dirname(os.getcwd())
-    PhonDir = os.path.dirname(projectDir)
-    newCorpusDir = os.path.join(projectDir, keyword)
-    
-    # Create new corpus folder from keyword
-    try:
-        os.mkdir(newCorpusDir)
-    except FileExistsError:
-        print(f"{keyword} directory already exists. Adding to folder")
-    
-    for File in os.listdir():
-        # If the name of the file contains a keyword
-        if fnmatch.fnmatch(File,'*' + keyword + '*'):
-            print(f"{File} Copied to {keyword} Directory")          
-            # If the file is truly a file...
-            if os.path.isfile(File):
-                # Copy that file to the new corpus directory            
-                try:
-                    shutil.copy(File,newCorpusDir)
-                except shutil.SameFileError:
-                    continue
-                    
-                
-    return PhonDir, newCorpusDir
-                
-            
-def organize_files_by_regex(keyword):
-    """Create a new corpus filtered to one of 1000s, 2000s, 3000s, etc.
 
-    Args:
-        keyword (_type_): Single digit to specify sessions. Example: (1)000s, (2)000s.
+def derive_corpus(
+    session_directory, corpus_filter, session_filter, pre_select=1, post_select=0
+):
+    with change_dir(session_directory):
+        parent_directory = os.path.dirname(session_directory)
+        if "project.xml" not in os.listdir(parent_directory):
+            raise FileNotFoundError("project.xml not found in parent project directory")
+        if pre_select == 1:
+            exclude_pre_list = [
+                e for e in os.listdir(session_directory) if re.search(r"Pre\s?\d", e)
+            ]
+        if pre_select == 2:
+            multi_pre_list = [
+                e for e in os.listdir(session_directory) if re.search(r"Pre\s?\d", e)
+            ]
+            exclude_pre_list = [e.replace(" 2", "") for e in multi_pre_list]
+        if post_select == 1:
+            exclude_post_list = [
+                e for e in os.listdir(session_directory) if re.search(r"Post\s?II", e)
+            ]
+        if post_select == 2:
+            multi_post_list = [
+                e for e in os.listdir(session_directory) if re.search(r"Post\s?II", e)
+            ]
+            exclude_post_list = [e.replace(" II", "") for e in multi_post_list]
+        # Create corpus folders
+        for corpus in corpus_filter:
+            try:
+                os.mkdir(os.path.join(parent_directory, corpus))
+            except FileExistsError:
+                print(f"{corpus} directory already exists. Adding to folder")
+        # Organize all files
+        for e in os.listdir(session_directory):
+            assert os.path.isfile(e), "Error: Subdirectory found."
+            for corpus in corpus_filter:
+                # Don't copy original Pre files when later pre files exist
+                if pre_select > 0:
+                    if e in exclude_pre_list:
+                        continue
+                if post_select > 0:
+                    if e in exclude_post_list:
+                        continue
+                if fnmatch.fnmatch(e, "*" + corpus + "*"):
+                    for key in session_filter:
+                        if fnmatch.fnmatch(e, "*" + key + "*"):
+                            shutil.copy(e, os.path.join(parent_directory, corpus, e))
 
-    Returns:
-        STR: String of corpus name.
-    """
-    # Working in corpus folder
-    projectDir = os.path.dirname(os.getcwd())
-    PhonDir = os.path.dirname(projectDir)
-    newCorpusName = keyword + '000s'
-    newCorpusDir = os.path.join(projectDir, newCorpusName)
-    
-    try: 
-        os.mkdir(newCorpusDir)
-    except FileExistsError:
-        print(f"{newCorpusName} directory already exists. Adding to folder")
-        
-    for File in os.listdir():
-        reg_exp = re.compile(keyword+r'\d{3}')
-        # If the name of the file contains a keyword
-        if re.search(reg_exp, File):
-            print(f"{File} Copied to {newCorpusName} Directory")        
-            # If the file is truly a file...
-            if os.path.isfile(File):
-                try:
-                    shutil.copy(File,newCorpusDir)
-                except shutil.SameFileError:
-                    continue
-    return newCorpusName
-                
-            
-def organize(corpusKey = None):
-    """Create new corpora based on groups of participant numbers or specified keyword.
-
-    Args:
-        corpusKey (STR, optional): String to use as corpus name and session filter. 
-        Defaults to None.
-
-    Returns:
-        None
-    """
-    # If no corpusKey is specified, sessions are grouped in corpuses
-    # by participant number (e.g., 1000s, 2000s, 3000s)
-    # This saves into the same folder as the current corpus.   
-    if corpusKey == None:
-        
-        corpusNames = []
-        for keyword in range(1,10):
-                keyword = str(keyword)
-                corpusNames.append(organize_files_by_regex(keyword))
-        
-        return corpusNames
-    
-    # Organize into corpora by keyword(s)
-    else:
-        if isinstance(corpusKey, str):
-            organize_files_by_keyword(corpusKey)
-        else:
-            for corpus in corpusKey:
-                organize_files_by_keyword(corpus)
-        
-        return
-
-def reorganizePhonProject(projectDir, corpusKey=None, filterKey=None):
-    """_summary_
-
-    Args:
-        projectDir (_type_): _description_
-        corpusKey (_type_, optional): _description_. Defaults to None.
-        filterKey (_type_, optional): _description_. Defaults to None.
-    """
-    with change_dir(os.path.normpath(projectDir)):
-
-        assert 'project.xml' in os.listdir(), "project.xml not found in project directory"
-        
-        # First sorting pass. Organize in participant number buckets.
-        for f in os.listdir():
-            if os.path.isdir(f):
-                # Enter corpus folder(s)
-                with enter_dir(f):
-                    if corpusKey == None:
-                        corpusKey = organize(corpusKey)
-                    else:
-                        organize(corpusKey)
-        # Second filter pass. Filter by filterKey.
-        if filterKey != None:            
-            
-            for corpus in corpusKey:
-                with enter_dir(corpus):
-                    print(f"Deleting {corpus} sessions not in filterKey...")
-                    for f in os.listdir():
-                        if any([fnmatch.fnmatch(f,'*' + fil + '*') for fil in filterKey]):
-                            continue
                         else:
-                            # Check if file exists:
-                            if os.path.isfile(f):
-                                os.remove(f)
-                            else:
-                                print(f"ERROR: {f} Not found in {corpus} Directory")
-                
-                    print(f"{len(os.listdir())} Phon sessions copied to {corpus} corpus.")
-    
+                            continue
+                else:
+                    continue
+
     return
 
 
 # Examples
-#reorganizePhonProject('DPA 3.0 - Original - Copy', ['Pre', 'Post'], ['PKP', 'OCP', 'CCP'])
-#reorganizePhonProject('DPA 3.0 - Original - Copy', filterKey=['PKP', 'GFTA'])
-reorganizePhonProject('/Users/pcombiths/Documents/DPA v1_4 all 2', filterKey=['PKP'])
+# reorganizePhonProject('DPA 3.0 - Original - Copy', ['Pre', 'Post'], ['PKP', 'OCP', 'CCP'])
+# reorganizePhonProject('DPA 3.0 - Original - Copy', filterKey=['PKP', 'GFTA'])
+# reorganizePhonProject('/Users/pcombiths/Documents/DPA v1_4 all 2', filterKey=['PKP'])
 
-
-#with change_dir(os.path.normpath(root_directory)):
-#    organize(7) # Set option number
-
+derive_corpus(
+    "/Users/pcombiths/Documents/DPA v1_4 all 2/Data",
+    ["Pre", "Post"],
+    ["PKP", "OCP", "CCP", "GFTA"],
+    pre_select=1,
+    post_select=0,
+)
 
 ### ToDo
 # Output to a new Phon project, rather than inside the same directory
 # Accept input from a Phon base project, rather than a corpus folder
 # Allow organizing/filtering by multiple layers
-
